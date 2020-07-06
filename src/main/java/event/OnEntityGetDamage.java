a25d9d36dbecfd74e9f2c4258334e5b64a4c5490
Vestri <p.prokop@firenja.com>
2020-07-06 21:21:44 +0200

    The player's food and effects are not reset after death fixed

--- src/main/java/event/OnEntityGetDamage.java
+++ src/main/java/event/OnEntityGetDamage.java
@@ -3,12 +3,12 @@ package main.java.event;
 import main.java.huntingground.HuntingGround;
 import main.java.huntingground.HuntingGroundManager;
 import org.bukkit.attribute.Attribute;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.potion.PotionEffect;
 
 import java.util.Objects;
 
@@ -27,6 +27,11 @@ public class OnEntityGetDamage implements Listener
                 {
                     p.teleport(hg.activePlayerSpawnpoint.loc);
                     p.setHealth(Objects.requireNonNull(p.getAttribute(Attribute.GENERIC_MAX_HEALTH)).getDefaultValue());
+                    p.setFoodLevel(20);
+                    for (PotionEffect effect : p.getActivePotionEffects())
+                    {
+                        p.removePotionEffect(effect.getType());
+                    }
                     hg.reduceGroupLive();
                     event.setCancelled(true);
                 }
